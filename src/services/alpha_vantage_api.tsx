096fecb4f2593d2db18264a7f56de01ce4f2cea2
Josheel <josheeldhanda@gmail.com>
2025-06-05 17:22:13 -0400

    placeholder for future, building this fully  main focus right now

--- src/services/alpha_vantage_api.tsx
+++ src/services/alpha_vantage_api.tsx
@@ -3,6 +3,22 @@ import { NormalizedStock, normalizeStockList } from '../types/Stock';
 const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = 'https://www.alphavantage.co/';
 
+export async function getStock(symbol: string): Promise<NormalizedStock[]> {
+    let url: string = `${BASE_URL}/query?function=SYMBOL_SEARCH&keywords=${encodeURIComponent(symbol)}&apikey=${API_KEY}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('API error: ${response.status');
+    }
+    const data = await response.json();
+    return normalizeStockList(data['bestMatches']);
+}
+
+export async function fetchStockOverview(symbol: string) {
+  const API_KEY = "your_api_key_here";
+  const res = await fetch(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${API_KEY}`);
+  const data = await res.json();
+  return data;
+}
 
 let cnq = [
     {
@@ -51,12 +67,57 @@ let cnq = [
     }
 ];
 
-export async function getStock(symbol: string): Promise<NormalizedStock[]> {
-    let url: string = `${BASE_URL}/query?function=SYMBOL_SEARCH&keywords=${encodeURIComponent(symbol)}&apikey=${API_KEY}`;
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error('API error: ${response.status');
-    }
-    const data = await response.json();
-    return normalizeStockList(data['bestMatches']);
-} 
+let cnqOverview = {
+    "Symbol": "CNQ",
+    "AssetType": "Common Stock",
+    "Name": "Canadian Natural Resources Ltd",
+    "Description": "Canadian Natural Resources Limited acquires, explores, develops, produces, markets and sells crude oil, natural gas and natural gas liquids (NGL). The company is headquartered in Calgary, Canada.",
+    "CIK": "1017413",
+    "Exchange": "NYSE",
+    "Currency": "USD",
+    "Country": "USA",
+    "Sector": "ENERGY & TRANSPORTATION",
+    "Industry": "CRUDE PETROLEUM & NATURAL GAS",
+    "Address": "2100, 855-2 STREET SW, CALGARY ALBERTA CANADA, AB, CA",
+    "OfficialSite": "https://www.cnrl.com",
+    "FiscalYearEnd": "December",
+    "LatestQuarter": "2025-03-31",
+    "MarketCapitalization": "64326492000",
+    "EBITDA": "18141999000",
+    "PERatio": "12.05",
+    "PEGRatio": "12.75",
+    "BookValue": "19.28",
+    "DividendPerShare": "2.2",
+    "DividendYield": "0.0556",
+    "EPS": "2.55",
+    "RevenuePerShareTTM": "18.13",
+    "ProfitMargin": "0.198",
+    "OperatingMarginTTM": "0.302",
+    "ReturnOnAssetsTTM": "0.0876",
+    "ReturnOnEquityTTM": "0.19",
+    "RevenueTTM": "38351000000",
+    "GrossProfitTTM": "19203000000",
+    "DilutedEPSTTM": "2.55",
+    "QuarterlyEarningsGrowthYOY": "1.543",
+    "QuarterlyRevenueGrowthYOY": "0.327",
+    "AnalystTargetPrice": "35.19",
+    "AnalystRatingStrongBuy": "6",
+    "AnalystRatingBuy": "8",
+    "AnalystRatingHold": "7",
+    "AnalystRatingSell": "0",
+    "AnalystRatingStrongSell": "0",
+    "TrailingPE": "12.05",
+    "ForwardPE": "12.74",
+    "PriceToSalesRatioTTM": "1.677",
+    "PriceToBookRatio": "2.216",
+    "EVToRevenue": "2.384",
+    "EVToEBITDA": "6.08",
+    "Beta": "1.519",
+    "52WeekHigh": "37.0",
+    "52WeekLow": "24.65",
+    "50DayMovingAverage": "29.5",
+    "200DayMovingAverage": "32.02",
+    "SharesOutstanding": "2093280000",
+    "DividendDate": "2025-07-03",
+    "ExDividendDate": "2025-06-13"
+};
